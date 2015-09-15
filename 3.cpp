ed30717692dc24cb753269ba73a9389825cd519b
ratasxy <juan.salas.santillana@ucsp.edu.pe>
2015-09-15 07:44:51 -0500

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <mpi.h>
+#include <string>
 
 #define MAX 50
 
@@ -20,7 +21,9 @@ int main(int argc, char** argv)
 {
     MPI_Init(NULL, NULL);
     string data;
-    int tmp;
+    int tmp, tmp2;
+    int size;
+    int sum = 0;
 
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -29,21 +32,28 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     if(world_rank == 0){
-	cin >> data;
+	getline(cin,data);
         tmp = 0;
-        for(int i=0;i<world_size;i++)
+	size = data.size();
+        for(int i=1;i<world_size-1;i++)
         {
-	  
+		MPI_Send(&size, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&data[tmp], size/(world_size-1), MPI_INT, i, 0, MPI_COMM_WORLD);
+
+		tmp += size/(world_size-1);
+
+		MPI_Recv(&tmp2, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		sum += tmp2;			  
         }
+	
+	cout << "Palabras: " << sum << endl;
     }else{
-	MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	MPI_Recv(&data, num, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Recv(&size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	MPI_Recv(&data, size/(world_size-1), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+	tmp2 = countString(data); 
+	MPI_Send(&tmp, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
-    
-    cout << "El proceso " << world_rank << " recibio ";
-    for(int i=0;i<num;i++)
-	cout << data[i] << " ";
-    cout << endl;
 
     MPI_Finalize();
 }
