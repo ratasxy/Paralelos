5a7ef51a3a6c759ad6c030398d399979bec305ea
ratasxy <juan.salas.santillana@ucsp.edu.pe>
2015-09-22 06:02:41 -0500

    Create mamulti.cpp

---
+++ mamulti.cpp
@@ -0,0 +1,77 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define SIZE 8
+
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
+
+void fill_matrix(int m[SIZE][SIZE])
+{
+  static int n=0;
+  int i, j;
+  for (i=0; i<SIZE; i++)
+    for (j=0; j<SIZE; j++)
+      m[i][j] = n++;
+}
+
+void print_matrix(int m[SIZE][SIZE])
+{
+  int i, j = 0;
+  for (i=0; i<SIZE; i++) {
+    printf("\n\t| ");
+    for (j=0; j<SIZE; j++)
+      printf("%2d ", m[i][j]);
+    printf("|");
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+  int myrank, P, from, to, i, j, k;
+  MPI_Status status;
+  
+  MPI_Init (&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &P);
+
+  if (SIZE%P!=0) {
+    if (myrank==0) printf("Matriz does not divisible\n");
+    MPI_Finalize();
+    return 1;
+  }
+
+  from = myrank * SIZE/P;
+  to = (myrank+1) * SIZE/P;
+
+  if (myrank==0) {
+    fill_matrix(A);
+    fill_matrix(B);
+  }
+
+  MPI_Bcast (B, SIZE*SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatter (A, SIZE*SIZE/P, MPI_INT, A[from], SIZE*SIZE/P, MPI_INT, 0, MPI_COMM_WORLD);
+
+  printf("computing slice %d (from row %d to %d)\n", myrank, from, to-1);
+  for (i=from; i<to; i++) 
+    for (j=0; j<SIZE; j++) {
+      C[i][j]=0;
+      for (k=0; k<SIZE; k++)
+	C[i][j] += A[i][k]*B[k][j];
+    }
+
+  MPI_Gather (C[from], SIZE*SIZE/P, MPI_INT, C, SIZE*SIZE/P, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (myrank==0) {
+    printf("\n\n");
+    print_matrix(A);
+    printf("\n\n\t       * \n");
+    print_matrix(B);
+    printf("\n\n\t       = \n");
+    print_matrix(C);
+    printf("\n\n");
+  }
+
+  MPI_Finalize();
+  return 0;
+}
